d8ca9d4d73a0bf3d92ba3b41fcb3da01b377d21e
jhartman <jhartman@sfu.ca>
2017-02-06 21:41:53 -0500

    Batch file to clean and run servers

--- RoomAliveToolkit_Build_1.0.bat
+++ RoomAliveToolkit_Build_1.0.bat
@@ -1,10 +1,47 @@
 title ProCam Ensemble Build Script
-
 @echo on
 
-@REM Setting msBuild Command: set msBuildDir=%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\Tools
-set msVCVarsDir=%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\Tools
+REM Check Command Line Vars
+SET BUILDTYPE="%1"
+IF %BUILDTYPE%=="" (
+  SET BUILDTYPE="DEBUG"
+  )
 
-@REM Call and set all environment variables for VS 2015
-@REM Evoke: call "%msVCVarsDir%\"vsvars32.bat
-call "%msVCVarsDir%\vsvars32.bat"
+REM Find Project Directory
+IF NOT DEFINED ROOMALIVETK (
+  REM PLEASE set up environment variable for Room alive tookkit.
+  exit /b
+  )
+
+REM Setting Env Vars
+SET msVCVarsDir=%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\Tools
+SET ProjectorServerDir=%ROOMALIVETK%\ProCamCalibration\ProjectorServer\bin\%BUILDTYPE%
+SET KinectServerDir=%ROOMALIVETK%\ProCamCalibration\KinectServer\bin\%BUILDTYPE%
+SET CurrentDir=%CD%
+
+REM Call and set all environment variables for VS 2015
+if not defined DevEnvDir (
+    CALL "%msVCVarsDir%\vsvars32.bat"
+
+)
+
+REM Killtasks
+taskkill /FI "IMAGENAME eq ProjectorServer.exe" /F
+taskkill /FI "IMAGENAME eq KinectServer.exe" /F
+
+CHDIR  %ROOMALIVETK%\ProCamCalibration
+IF %BUILDTYPE%=="DEBUG" (
+  REM Debug Build
+  CALL MSBuild ProCamEnsembleCalibration.sln /p:Configuration=Debug /t:Clean;Rebuild /l:FileLogger,Microsoft.Build.Engine;logfile=../Manual_MSBuild_Debug_LOG.log
+  )
+
+IF %BUILDTYPE%=="RELEASE" (
+  REM Release Build
+  CALL MSBuild ProCamEnsembleCalibration.sln /p:Configuration=Release /t:Clean;Rebuild /l:FileLogger,Microsoft.Build.Engine;logfile=../Manual_MSBuild_Release_LOG.log
+  )
+
+REM Run Servers
+START %ProjectorServerDir%\ProjectorServer.exe
+START %KinectServerDir%\KinectServer.exe
+CHDIR %CurrentDir%
+exit
